e29d52d416005c3de38d4591a0d09091dfdbf631
hazel <hazel@meows.zip>
2026-08-07 19:18:04 -0400

    jujutsu

--- home/utils.nix
+++ home/utils.nix
@@ -41,6 +41,31 @@
     ];
   };
 
+  programs.jujutsu = {
+    enable = true;
+    settings = {
+      user = {
+        name = "hazel";
+        email = "hazel@meows.zip";
+      };
+      revsets = {
+        bookmark-advance-to = "closest_pushable(@)";
+      };
+      revset-aliases = {
+        "closest_pushable(to)" = "heads(::to & mutable() & ~description(exact:'') & (~empty() | merges()))";
+      };
+      aliases = {
+        recent = [ "bookmark" "list" "--sort=committer-date-" "--color=always" ];
+        bookmark-create-tracked = [
+          "util" "exec" "--"
+          "sh" "-c"
+          "jj bookmark create \"$1\" -r 'closest_pushable(@)' && jj bookmark track \"$1\" --remote origin"
+          "_"
+        ];
+      };
+    };
+  };
+
   programs.direnv = {
     enable = true;
     enableZshIntegration = true;
